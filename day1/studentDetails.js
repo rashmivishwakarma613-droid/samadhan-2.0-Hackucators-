d95e36eb5b4efc1575350ecdaf4974e1b6f1a4f3
rashmivishwakarma613-droid <rashmivishwakarma613@gmail.com>
2025-08-27 10:44:22 +0530

    Create studentDetails.js

---
+++ day1/studentDetails.js
@@ -0,0 +1,10 @@
+onst student = {
+  name: "Ankit vishwakarma", 
+  age: 18,              
+  grade: "12th"          
+};
+
+console.log("Student Details:");
+console.log("Name: " + student.name);  
+console.log("Age: " + student.age);     
+console.log("Grade: " + student.grade);
